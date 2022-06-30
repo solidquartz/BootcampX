57c63cc09d60f2423e556cf1b0e227d1b9c0a014
Aky Mac Donald <kotoko@Kotoko.local>
2022-06-30 16:22:44 -0400

    Add node postgreSQL

--- 4_queries/12_name_of_teachers_who_assisted.sql
+++ 4_queries/12_name_of_teachers_who_assisted.sql
@@ -1,8 +1,7 @@
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM assistance_requests
-JOIN teachers ON teachers.id = teacher_id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
-GROUP BY cohort, teacher
 ORDER BY teacher;
