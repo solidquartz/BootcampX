3b9e22fc491fa7e6ca2154e474a2c12968fc1061
Aky Mac Donald <kotoko@Kotoko.local>
2022-06-30 16:58:32 -0400

    Protect against SQL injection attacks

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -4,17 +4,22 @@ const pool = new Pool({
   user: 'kotoko',
   password: '123',
   host: 'localhost',
+
 });
 
-pool.query(`
+const cohortName = process.argv[2] || 'JUL02';
+const values = [cohortName];
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
